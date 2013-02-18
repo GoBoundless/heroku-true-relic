e7dba06cdf2a42ca00c61127b91e92d5d7676bf2
Matt Hodgson <matthodgson@mac.com>
2013-02-18 18:46:11 -0500

    insert didn't work

--- lib/heroku-true-relic/railtie.rb
+++ lib/heroku-true-relic/railtie.rb
@@ -1,7 +1,7 @@
 module HerokuTrueRelic
   class Railtie < Rails::Railtie
     initializer 'heroku_true_relic.add_queue_time_logger' do |app|
-      app.config.middleware.insert 0, "HerokuTrueRelic::QueueTimeLogger"
+      app.config.middleware.ansert_after "Rack::Runtime", "HerokuTrueRelic::QueueTimeLogger"
     end
   end
 end
